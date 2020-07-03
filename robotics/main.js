b9f07074232227b1bb2149de08718ef2c6fe0647
portfolioprime <65928579+portfolioprime@users.noreply.github.com>
2020-07-03 14:08:31 +0200

    Add files via upload
    Added back-to-portfolio Button

--- robotics/main.js
+++ robotics/main.js
@@ -1,3 +1,4 @@
+//Option 1 - Open Nav  
 // Select element function
 const selectElement = function (element) {
     return document.querySelector(element);
@@ -6,15 +7,44 @@ const selectElement = function (element) {
 let menuToggler = selectElement('.menu-toggle');
 let body = selectElement('body');
 
-// Open/Close menu on .menu-toggle click
+// 1. Open/Close menu on .menu-toggle click
 menuToggler.addEventListener('click', function () {
     body.classList.toggle('open');
 });
 
-// Select all nav elements function 
+// 2. Select all nav elements function 
 document.querySelectorAll('.nav a').forEach(el => el.addEventListener('click', function () {
     body.classList.remove('open');
 }));
+
+
+
+// 3. Select back-to-portfolio button function 
+let goBack = selectElement('.back-to-portfolio');
+
+// Open/Close back-to-portfolio on .back-to-portfolio click
+goBack.addEventListener('click', function () {
+  body.classList.toggle('go-back');
+});
+
+
+// //Option 2
+// // Select element function
+// const selectElement = (element) =>
+//   document.querySelector(element);
+// const getAllWithClass = (className) =>
+//   document.getElementsByClassName(className);
+
+// let body = selectElement('body');
+// let menuClosers = Array.from(getAllWithClass("nav-link"));
+
+// // Close menu on .nav-link click
+// menuClosers.forEach(closer => {
+//   closer.addEventListener('click', function() {
+//     body.classList.remove('open');
+//     // console.log("(No more blue background means it's closed)");
+//   });
+// });
 
 // let menuClose = selectElement('.nav-link');
 

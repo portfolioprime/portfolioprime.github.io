a322bc4193ed778891d8405741e33608cf092ffb
portfolioprime <65928579+portfolioprime@users.noreply.github.com>
2020-07-03 20:41:54 +0200

    Add files via upload
    Added back to portfolio button w overlay on click

---
+++ jokes/main.js
@@ -0,0 +1,16 @@
+//Option 1 - Open Nav  
+// Select element function
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+// Define Vars
+let body = selectElement('body');
+
+// 3. Select back-to-portfolio button function 
+let goBack = selectElement('.back-to-portfolio');
+
+// Open/Close back-to-portfolio on .back-to-portfolio click
+goBack.addEventListener('click', function () {
+  body.classList.toggle('go-back');
+});

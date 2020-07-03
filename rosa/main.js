dea67fc44d14c2067d2f64be87029782cb80341a
portfolioprime <65928579+portfolioprime@users.noreply.github.com>
2020-07-03 14:54:44 +0200

    Add files via upload
    Added back to portfolio button

--- rosa/main.js
+++ rosa/main.js
@@ -3,12 +3,24 @@ const selectElement = function (element) {
     return document.querySelector(element);
 };
 
+
+// 1a. Define Variables for menu-toggle and body
 let menuToggler = selectElement('.menu-toggle');
 let body = selectElement('body');
 
+// 1b. Open/Close back-to-portfolio Button
 menuToggler.addEventListener('click', function () {
     body.classList.toggle('open');
 });
+
+// 2a. Define Variables for back-to=portfolio button
+let goBack = selectElement('.back-to-portfolio');
+
+// 2b. Open/Close back-to-portfolio on .back-to-portfolio click
+goBack.addEventListener('click', function () {
+  body.classList.toggle('go-back');
+});
+
 
 //Scroll reveal
 window.sr = ScrollReveal();
